15dd945a2cd3c8e92f7060ec41993a07efd3fff4
jon-vdb <jonvdberg8@gmail.com>
2024-03-21 09:48:22 +0700

    add pwa

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,20 @@
 import Metronome from "@/app/components/Metronome";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Metronome/>
-    </main>
-  );
+    if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+        window.addEventListener('load', async () => {
+            try {
+                const registration = await navigator.serviceWorker.register('/service-worker.js');
+                console.log('Service worker registered:', registration);
+            } catch (error) {
+                console.error('Service worker registration failed:', error);
+            }
+        });
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Metronome />
+        </main>
+    );
 }
