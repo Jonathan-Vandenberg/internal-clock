f3b8050d412686b2aac9bbd713fa764b5c0178f2
jon-vdb <jonvdberg8@gmail.com>
2024-03-21 10:06:32 +0700

    edit serveice worker 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,39 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('my-cache').then((cache) => {
-            return cache.addAll(['/', '/index.html', '/manifest.json', '/android/*.png', '/ios/*.png'])
+            const androidImages = [
+                '/android/40.png',
+                '/android/80.png',
+                '/android/120.png',
+                '/android/256.png',
+            ];
+            const iosImages = [
+                '/ios/40.png',
+                '/ios/80.png',
+                '/ios/120.png',
+                '/ios/256.png',
+            ];
+
+            const allImages = [...androidImages, ...iosImages];
+
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                ...allImages
+            ]).catch(error => {
+                console.error('Cache addAll error:', error);
+            });
         })
-    )
-})
+    );
+});
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request)
+            return response || fetch(event.request).catch(error => {
+                console.error('Fetch error:', error);
+            });
         })
-    )
-})
+    );
+});
